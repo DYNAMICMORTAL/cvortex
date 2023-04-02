fc93f002601c60d79cc311af32fd3698ab509375
Mihir <85486112+DYNAMICMORTAL@users.noreply.github.com>
2023-04-02 11:22:15 +0530

    Bubble Sort and Selection Sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,14 +1,14 @@
 public class bubbleSort {
     public static void printArray(int arr[]){
         for(int i =0; i<arr.length; i++){
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
-        System.out.println();
     }
     public static void main(String[] args){
         int arr[] = {7,9,8,5,10};
+        // Ascending order of Numbers:
         for(int i=0; i<arr.length-1; i++){
-            for (int j = 0;j< arr.length-i-1; j++){
+            for (int j = 0;j< arr.length-i-1; j++){ // this condition of j<arr.length-i-1 is to ignore the last already ordered numbers.
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -16,6 +16,19 @@ public class bubbleSort {
                 }
             }
         }
+        printArray(arr);
+
+        // Descending order of Numbers:
+        for(int k = 0; k<arr.length-1; k++){
+            for(int l=0; l<arr.length-k-1; l++){
+                if(arr[l]<arr[l+1]){
+                    int temp2 = arr[l+1];
+                    arr[l+1] = arr[l];
+                    arr[l] = temp2;
+                }
+            }
+        }
+        System.out.println("\nNumbers in descending order are: ");
         printArray(arr);
     }
 }
